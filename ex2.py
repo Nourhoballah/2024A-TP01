38b3bfa2738f987d7af43693a571b0861452b74c
Nourhoballah <nourhoballah@outlook.com>
2024-09-15 00:33:10 -0400

    fix #2

--- ex2.py
+++ ex2.py
@@ -3,16 +3,15 @@
 
 import math
 
-water_quantity = int(input("Quelle quantité d'eau faut-il assainir ? ") )
-x=water_quantity
+water_quantity = float(input("Quelle quantité d'eau faut-il assainir ? " ) )
+x = water_quantity
 
-filtreNecessaire= (x*1)/5
-lampeNecessaire= (x*3)/5
+filtreNecessaire= math.ceil((x*1)/5)
+lampeNecessaire= math.ceil((x*3)/5)
 chloreNecessaire=(x*0.5)/5
 
-print("Voici les matériaux requis pour l'assainissement de", x,   "L d'eau :" ,filtreNecessaire ,"filtres, ",lampeNecessaire, "lampes UV ""et" ,chloreNecessaire, "kg de chlore" )
-#print("la quantité nécessaire de filtre est de " filtreNecessaire)
-#print("la quantité nécessaire de filtre est de " lampeNecessaire)
-#print("la quantité nécessaire de filtre est de " chloreNecessaire)
-
+print(f"Voici les éléments requis pour assainir {x:.1f}L d'eau:\n        ")
+print(f"\t- Filtre(s) : {filtreNecessaire}" )
+print(f"\t- Lampe(s) UV : {lampeNecessaire}")
+print(f"\t- Chlore : {chloreNecessaire:.1f}kg")
 
